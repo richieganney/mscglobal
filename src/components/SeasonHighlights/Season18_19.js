8dde181c9dc834d829dac4b8e9c6d9709fbed7c4
richieganney <richieganney@gmail.com>
2019-10-03 14:41:02 +0100

    updated video gallery

--- src/components/SeasonHighlights/Season18_19.js
+++ src/components/SeasonHighlights/Season18_19.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import '../../styles.css'
 import Titanic from "../../photos_and_videos/titanic.mp4";
 import RacketGate from "../../photos_and_videos/racketGate.mp4";
@@ -24,64 +23,64 @@ class Season18_19 extends Component {
                 
                   <h1>Some of the magic from the 2018/2019 season</h1>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={Titanic} >
-                        <img class="videoThumb" src={vid5}></img></a>
+                        <a className="fancybox fancybox.iframe" href={Titanic} >
+                        <img className="videoThumb" src={vid5}></img></a>
                         </figure>
                         <h2>Titanic Rally</h2>
                         <p>Commonly dubbed the 'titanic rally', this fierce battle between Clive and Ned resulted in a draw, with both teams taking home a valubale point. As followers of the league will know, these two teams had to retire shortly after ths match. </p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={RacketGate}>
-                        <img class="videoThumb" src={vid6}></img></a>
+                        <a className="fancybox fancybox.iframe" href={RacketGate}>
+                        <img className="videoThumb" src={vid6}></img></a>
                         </figure>
                         <h2>Racketgate</h2>
                         <p>Ah racketgate. A lot of debate still surrounds the lack of punishment given to Rovers. Peter Brumby came under massive scrutiny following this event but thankfully game regulation has tightened since then.</p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={brumbiesUpset}>
-                        <img class="videoThumb" src={vid4}></img></a>
+                        <a className="fancybox fancybox.iframe" href={brumbiesUpset}>
+                        <img className="videoThumb" src={vid4}></img></a>
                         </figure>
                         <h2>Sad Brumbies</h2>
-                        <p>Here is a video of Brumbies looking very sad after throwing away an absolute sitter.</p>
+                        <p>Here is a video of the brumbies looking very sad after throwing away an absolute sitter.</p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={ottersVmangos}>
-                        <img class="videoThumb" src={vid3}></img></a>
+                        <a className="fancybox fancybox.iframe" href={ottersVmangos}>
+                        <img className="videoThumb" src={vid3}></img></a>
                         </figure>
                         <h2>Watergate</h2>
-                        <p>Otters V Mangos. This is to remind fans of Watergate, a much more horrific and well known situation than 'Watergate' during Nixons administration. Unfortunately this is not actual footage of Watergate.</p>
+                        <p>Otters V Mangos. This is to remind fans of Watergate, a much more horrific and well known situation than Nixon's Watergate. Unfortunately this is not actual footage of Watergate.</p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={EsamPackersFinal}>
-                        <img class="videoThumb" src={vid2}></img></a>
+                        <a className="fancybox fancybox.iframe" href={EsamPackersFinal}>
+                        <img className="videoThumb" src={vid2}></img></a>
                         </figure>
                         <h2>Build up</h2>
-                        <p>Build up to the Esam v Packers playoff semi-final.</p>
+                        <p>Build up to the Esam v Packers playoff final.</p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={PackersBrumbies}>
-                        <img class="videoThumb" src={vid1}></img></a>
+                        <a className="fancybox fancybox.iframe" href={PackersBrumbies}>
+                        <img className="videoThumb" src={vid1}></img></a>
                         </figure>
                         <h2>Packers v Brumbies</h2>
                         <p>Packers v Brumbies final point.</p>
                   </article>
 
-                  <article class="video">
+                  <article className="video" style={articleStyle}>
                         <figure>
-                        <a class="fancybox fancybox.iframe" href={TitanicWithMusic}>
-                        <img class="videoThumb" src={vid7}></img></a>
+                        <a className="fancybox fancybox.iframe" href={TitanicWithMusic}>
+                        <img className="videoThumb" src={vid7}></img></a>
                         </figure>
                         <h2>Titanic Rally (with music)</h2>
                         <p>The fantastic 'titanic rally' with actual titanic music.</p>
@@ -89,6 +88,10 @@ class Season18_19 extends Component {
             </div>
         );
     }
+}
+
+const articleStyle = {
+    height: '400px'
 }
 
 export default Season18_19;
