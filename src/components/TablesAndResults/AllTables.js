185cb9b37eef60553194109a4aed91f3444ec223
Richie <richieganney@gmail.com>
2019-11-04 09:29:59 +0000

    changing local repos

--- src/components/TablesAndResults/AllTables.js
+++ src/components/TablesAndResults/AllTables.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
 import TablesAndResults18_19 from './TablesAndResults18_19';
 import TablesAndResults19_20 from './TablesAndResults19_20';
-import Results18_19 from './Results18_19'
-import Results19_20 from './Results19_20';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
 
 class AllTables extends Component {
     render() {
-        return (
-            <div>
-                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                <Tab eventKey="tables" title="Tables">
-                <h1>South West Division 3 Tables</h1><br></br>
-                <TablesAndResults19_20/>
-                <TablesAndResults18_19/>
-                </Tab>
-                
-                <Tab eventKey="results" title="Results">
-                <Results19_20 />
-                <Results18_19 />
-                </Tab>
-                </Tabs>
+        return ( < div >
+            <h1> South West Division 3 Tables </h1><br></br >
+            <TablesAndResults19_20 / >
+            <TablesAndResults18_19 / >
             </div>
         );
     }
