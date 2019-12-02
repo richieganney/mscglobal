da4eb171f83ee9a5d433626e02bce5e917d730c0
Richie <richieganney@gmail.com>
2019-12-02 16:17:26 +0000

    function now displays the json in the correct order. need to map over it and pass it down as props to the header doesnn't duplicate

--- src/components/API_test.js
+++ src/components/API_test.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Table } from 'react-bootstrap';
 
+const compareFunction = (a, b) => {
+    if (a.points === b.points) {
+        if (a.points_difference === b.points_difference) {
+            return a.points_for < b.points_for ? 1 : -1
+        }
+        else {
+            return a.points_difference < b.points_difference ? 1 : -1
+        }
+    }
+    else {
+        return a.points < b.points ? 1 : -1
+    } 
+}
+
 class API_test extends Component {
     constructor(props) {
     super(props);
     this.state =
     {
-    table: [],
-    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    table: []
     };
     this.onClick = this.onClick.bind(this);
 }
@@ -19,17 +32,13 @@ onClick(e) {
     axios
     .get(url)
     .then(response => {
-        this.setState({ table: response.data })
-        console.log("RESPONSE")
-        console.log(response.data)
-        console.log("STATE")
-        console.log(this.state.table)
+        this.setState({ table: response.data.sort(compareFunction) })
     })
     .catch(error => {
-        console.log("THIS IS THE ERROR")
         console.log(error.response);
     });
 }
+// you will need to pass this down as props so that there is not an infinate loop
 render() {
     
     return (
@@ -37,35 +46,34 @@ render() {
             <center><button onClick={e => {this.onClick(e)}}>View Table</button></center>
             <h2>2019/2020</h2>
             <div>
-                <Table responsive>
-            <thead>
-                <tr>
-                <th></th>
-                <th>Team</th>
-                <th>Played</th>
-                <th>Win</th>
-                <th>Draw</th>
-                <th>Loss</th>
-                <th>Points For</th>
-                <th>Points Against</th>
-                <th>Points Difference</th>
-                <th>Points</th>
-                </tr>
-            </thead>
-            </Table>
+                
                 {this.state.table.map(player => (
-                             <Table responsive>
+                        <Table>
+                                <thead>
+                                <tr>
+                                <th></th>
+                                <th>Team</th>
+                                <th>Played</th>
+                                <th>Win</th>
+                                <th>Draw</th>
+                                <th>Loss</th>
+                                <th>Points For</th>
+                                <th>Points Against</th>
+                                <th>Points Difference</th>
+                                <th>Points</th>
+                                </tr>
+                            </thead>
                              <tbody>
-                             <tr style={style}>
-                                 <td>1</td>
+                             <tr>
+                                 <td></td>
                                  <th>{player.team_name}</th>
                                  <td>{player.played}</td>
                                  <td>{player.win}</td>
                                  <td>{player.draw}</td>
                                  <td>{player.loss}</td>
-                                 <td>{player.played}</td>
                                  <td>{player.points_for}</td>
                                  <td>{player.points_against}</td>
+                                 <td>{player.points_difference}</td>
                                  <td>{player.points}</td>
                              </tr>
                              </tbody>
