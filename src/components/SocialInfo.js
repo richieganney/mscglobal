7a1d172f3738511383b6c0a0fba906e648f978d8
Richie <richieganney@gmail.com>
2020-01-16 18:40:16 +0000

    adding social info page

---
+++ src/components/SocialInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SocialInfo extends Component {
+    render() {
+        return (
+            <div>
+               <center><h1>Coming Soon</h1></center>
+            </div>
+        );
+    }
+}
+
+export default SocialInfo;
